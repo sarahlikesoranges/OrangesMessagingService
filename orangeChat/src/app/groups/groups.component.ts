df69b24a994c88f269dd1cc640de5bc30cd843f8
sarahlikesoranges <109631514+sarahlikesoranges@users.noreply.github.com>
2022-09-11 15:45:54 +1000

    Happy with the groups layout page displays user details, edit user details displays groups

--- orangeChat/src/app/groups/groups.component.ts
+++ orangeChat/src/app/groups/groups.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsComponent implements OnInit {
 
+  username : string = "Sarah's Oranges";
+  usertype : string = "superduperadmin";
+  name : string = "sarah";
+  email: string = 'sarah@oranges.com';
+  groups:[] = [];
+  groupname :string = "myFirstGroup";
+  groupmember1: string = "member1-ally"
+  groupmember2: string = "member2-sam"
+  groupmember3: string = "member3-joe"
+  groupmember4: string = "member4-brit"
+
   constructor() { }
 
   ngOnInit(): void {
