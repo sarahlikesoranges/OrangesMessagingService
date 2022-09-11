7f5ec7f650ffb78709f1fe11ccf04ded6bf491bd
sarahlikesoranges <109631514+sarahlikesoranges@users.noreply.github.com>
2022-09-11 16:16:52 +1000

    added a channel layout and component

--- orangeChat/src/app/app-routing.module.ts
+++ orangeChat/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { GroupsComponent } from './groups/groups.component';
+import { ChannelComponent } from './channel/channel.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent },
-  {path: 'groups', component: GroupsComponent}
+  {path: 'groups', component: GroupsComponent},
+  {path: 'channel', component: ChannelComponent}
 ];
 
 @NgModule({
--- orangeChat/src/app/channel/channel.component.ts
+++ orangeChat/src/app/channel/channel.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./channel.component.css']
 })
 export class ChannelComponent implements OnInit {
-
+  groupname: string = "sassygroup";
+  groupmember1: string = "member1-ally"
+  groupmember2: string = "member2-sam"
+  groupmember3: string = "member3-joe"
+  groupmember4: string = "member4-brit"
+  message: string = ""
   constructor() { }
 
   ngOnInit(): void {
+  }
+  sendMessage(){
+    
   }
 
 }
