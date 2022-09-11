71cf807acc06466fa3fef4a803bfceaca3ec1c95
sarahlikesoranges <109631514+sarahlikesoranges@users.noreply.github.com>
2022-09-11 14:47:44 +1000

    added a channel component

--- orangeChat/src/app/app.module.ts
+++ orangeChat/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { GroupsComponent } from './groups/groups.component';
+import { ChannelComponent } from './channel/channel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    GroupsComponent
+    GroupsComponent,
+    ChannelComponent
   ],
   imports: [
     BrowserModule,
