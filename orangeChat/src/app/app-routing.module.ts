ab12c6c6c57e0433f4cd3f98ee2bd03660da5c86
sarahlikesoranges <109631514+sarahlikesoranges@users.noreply.github.com>
2022-09-11 15:20:26 +1000

    added a layout to groups page added a route for groups added a link to groups on the app html

--- orangeChat/src/app/app-routing.module.ts
+++ orangeChat/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { GroupsComponent } from './groups/groups.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent }
+  {path: '', component: LoginComponent },
+  {path: 'groups', component: GroupsComponent}
 ];
 
 @NgModule({
